237fbb3b422106c0c44013056e184e69493c80c9
Rezekoard <rezkyardianto1222@gmail.com>
2024-07-31 03:49:51 +0700

    Codelab Kirim Data dengan Bundle

--- app/src/main/java/com/rezekoard/mynavigation/CategoryFragment.kt
+++ app/src/main/java/com/rezekoard/mynavigation/CategoryFragment.kt
@@ -5,22 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.rezekoard.mynavigation.databinding.FragmentCategoryBinding
 
 
 class CategoryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding : FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
 
+    companion object{
+        val EXTRA_NAME = "extra_name"
+        val EXTRA_STOCK =  "extra_stock"
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCategoryLifestyle.setOnClickListener { view ->
+            val mBundle = Bundle()
+            mBundle.putString(EXTRA_NAME, "LifeStyle")
+            mBundle.putLong(EXTRA_STOCK, 7)
+            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle) }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
 
 
 }
